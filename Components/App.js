a6b5f8cd6e50a5427e13c8ca0bb9672f2e9cf1c7
Kayla <kayla.larson1990@gmail.com>
2019-09-01 17:08:24 -0600

    first commit

---
+++ Components/App.js
@@ -0,0 +1,98 @@
+import React from "react";
+import Card from "./Card";
+import { ScrollView, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
+import { NotificationIcon } from "./Icons";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <SafeAreaView>
+          <ScrollView>
+            <TitleBar>
+              <Avatar source={require("../assets/avatar.jpg")} />
+              <Title>Welcome back,</Title>
+              <Name>Kay</Name>
+              <NotificationIcon
+                style={{ position: "absolute", right: 20, top: 5 }}
+              />
+            </TitleBar>
+            <Subtitle>Continue Learning</Subtitle>
+            <ScrollView
+              horizontal={true}
+              style={{ paddingBottom: 30 }}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Card
+                title="Styled Components"
+                image={require("../assets/background2.jpg")}
+                caption="React Native"
+                logo={require("../assets/logo-react.png")}
+                subtitle="5 of 12 sections"
+              />
+              <Card
+                title="Styled Components 2"
+                image={require("../assets/background1.jpg")}
+                caption="React Native"
+                logo={require("../assets/logo-react.png")}
+                subtitle="5 of 12 sections"
+              />
+              <Card
+                title="Styled Components 3"
+                image={require("../assets/background3.jpg")}
+                caption="React Native"
+                logo={require("../assets/logo-react.png")}
+                subtitle="5 of 12 sections"
+              />
+            </ScrollView>
+          </ScrollView>
+        </SafeAreaView>
+      </Container>
+    );
+  }
+}
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
